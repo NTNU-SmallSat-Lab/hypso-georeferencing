dc3dc9eda406cff8fc7e7cbfcabcc131d9c09f46
Cameron P <cameron.l.penne@ntnu.no>
2024-07-18 13:16:05 -0500

    Update testing script

--- example_mode_change.py
+++ example_mode_change.py
@@ -9,9 +9,7 @@ import os
 
 
 filename = '/home/cameron/Nedlastinger/Frohavet/frohavet_2024-04-15_1006Z-bin3-original.points'
-gcps = georeferencing.GCPList(filename)
-
-#print(gcps)
+gcps = georeferencing.GCPList(filename, origin_mode='qgis', cube_width=598, cube_height=1092)
 
 print(gcps.image_mode)
 
@@ -19,8 +17,11 @@ gcps.change_image_mode(dst_image_mode='standard')
 
 print(gcps.image_mode)
 
+
+gcps.change_origin_mode(dst_origin_mode='cube')
+
 print(gcps.origin_mode)
 
-#gcps.save()
+gcps.save()
 
 
